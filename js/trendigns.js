2fb91d01bd49d5336bd5caaf2fcab216ec484fa3
Baral Alan <abaral@andreani.com>
2020-10-03 17:32:24 -0300

    cambios descarga de gif

--- js/trendigns.js
+++ js/trendigns.js
@@ -4,6 +4,7 @@ var offset = 0;
 var container = document.getElementById('trend_container')
 let previous = document.getElementById('previous')
 let next = document.getElementById('next')
+var file= {};
 
 previous.addEventListener('click', function () {
     if (offset != 0) {
@@ -146,7 +147,19 @@ function getTrendings() {
                 btn_descarga.classList.add("boton-descarga");
                 btn_descarga.id = "btn-descarga";
                 btn_descarga.appendChild(imagen_btn_descarga);
+
+                console.log("Qué viene en img", img);
+                console.log("Qué viene en info id", info.data[index].id);
+                btn_descarga.addEventListener('click', function() {
+                    async function prueba() {
+                        let blob = await fetch(url).then(r => r.blob());
+                        invokeSaveAsDialog(blob);
+                    }
+                    prueba();
+                })
+
                 imagen_btn_descarga.setAttribute("src", "Assets/icon-download.svg")
+                
                 /* FIN BOTON DE DESCARGA */
 
 
@@ -167,4 +180,25 @@ function getTrendings() {
         })
      
 }
+async function prueba(url) {
+    var response = await fetch(url);
+    file = await response.blob();
+}
+
+// (async () => {
+//     //create new a element
+//     let a = document.createElement('a');
+//     // get image as blob
+//     let response = await fetch('https://media2.giphy.com/media/DvyLQztQwmyAM/giphy.gif?cid=e9ff928175irq2ybzjyiuicjuxk21vv4jyyn0ut5o0d7co50&rid=giphy.gif');
+//     let file = await response.blob();
+//     // use download attribute https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a#Attributes
+//     a.download = 'myGif';
+//     a.href = window.URL.createObjectURL(file);
+//     //store download url in javascript https://developer.mozilla.org/en-US/docs/Learn/HTML/Howto/Use_data_attributes#JavaScript_access
+//     a.dataset.downloadurl = ['application/octet-stream', a.download, a.href].join(':');
+//     //click on element to start download
+//     a.click();
+//   })();
+
+
 getTrendings();
