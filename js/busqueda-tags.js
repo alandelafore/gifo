d450d28db656ed671768c4aa2ec15aecdfd3029b
unknown <eze.rey92@gmail.com>
2021-02-08 22:21:47 -0300

    casi todo

--- js/busqueda-tags.js
+++ js/busqueda-tags.js
@@ -1,61 +1,68 @@
 
-var trend_01    =document.getElementById("trend-01");
-var trend_02    =document.getElementById("trend-02");
-var trend_03    =document.getElementById("trend-03");
-var trend_04    =document.getElementById("trend-04");
-var trend_05    =document.getElementById("trend-05");
+var trend_01    = document.getElementById("trend-01");
+var trend_02    = document.getElementById("trend-02");
+var trend_03    = document.getElementById("trend-03");
+var trend_04    = document.getElementById("trend-04");
+var trend_05    = document.getElementById("trend-05");
 let cruz_buscar = document.getElementById("btn-borrar");
-let lupas        = document.getElementById("lupa-derecha");
+let lupas       = document.getElementById("lupa-derecha");
 
 
-trend_01.addEventListener("click",function (ev) {
-  let texto = trend_01.textContent;
-  let resultado = texto.split(",",1);
- busqueda=document.getElementById("busqueda").value =resultado;
- fetch_busqueda();
- cruz_buscar.classList.remove("ocultar");
- lupas.classList.add("ocultar");
-  
-})
-trend_02.addEventListener("click",function (ev) {
-  let texto = trend_02.textContent;
-  let resultado = texto.split(",",1);
-  busqueda=document.getElementById("busqueda").value =resultado;
- lupas.classList.add("ocultar");
+trend_01.addEventListener("click", function (ev) {
+  let texto     = trend_01.textContent;
+  let resultado = texto.split(",", 1);
 
-  
+  busqueda      = document.getElementById("busqueda").value = resultado;
+
   fetch_busqueda();
- cruz_buscar.classList.remove("ocultar");
 
-  
-})
-trend_03.addEventListener("click",function (ev) {
-  let texto = trend_03.textContent;
-  let resultado = texto.split(",",1);
- busqueda=document.getElementById("busqueda").value =resultado;
- fetch_busqueda();
- cruz_buscar.classList.remove("ocultar");
- lupas.classList.add("ocultar");
+  cruz_buscar.classList.remove("ocultar");
 
-  
+  lupas.classList.add("ocultar");
+
 })
-trend_04.addEventListener("click",function (ev) {
+trend_02.addEventListener("click", function (ev) {
+  let texto     = trend_02.textContent;
+  
+  let resultado = texto.split(",", 1);
+  
+  busqueda      = document.getElementById("busqueda").value = resultado;
+  
+  lupas.classList.add("ocultar");
+
+
+  fetch_busqueda();
+  cruz_buscar.classList.remove("ocultar");
+
+
+})
+trend_03.addEventListener("click", function (ev) {
+  let texto     = trend_03.textContent;
+  let resultado = texto.split(",", 1);
+  busqueda     = document.getElementById("busqueda").value = resultado;
+  fetch_busqueda();
+  cruz_buscar.classList.remove("ocultar");
+  lupas.classList.add("ocultar");
+
+
+})
+trend_04.addEventListener("click", function (ev) {
   let texto = trend_04.textContent;
-  let resultado = texto.split(",",1);
- busqueda=document.getElementById("busqueda").value =resultado;
- fetch_busqueda();
- cruz_buscar.classList.remove("ocultar");
- lupas.classList.add("ocultar");
+  let resultado = texto.split(",", 1);
+  busqueda = document.getElementById("busqueda").value = resultado;
+  fetch_busqueda();
+  cruz_buscar.classList.remove("ocultar");
+  lupas.classList.add("ocultar");
 
 
 })
-trend_05.addEventListener("click",function (ev) {
+trend_05.addEventListener("click", function (ev) {
   let texto = trend_05.textContent;
-  let resultado = texto.split(",",1);
- busqueda=document.getElementById("busqueda").value =resultado;
- fetch_busqueda();
- cruz_buscar.classList.remove("ocultar");
- lupas.classList.add("ocultar");
+  let resultado = texto.split(",", 1);
+  busqueda = document.getElementById("busqueda").value = resultado;
+  fetch_busqueda();
+  cruz_buscar.classList.remove("ocultar");
+  lupas.classList.add("ocultar");
 
 
 })
@@ -71,15 +78,15 @@ function fetch_busqueda2() {
     .then((respuesta) => respuesta.json())
     .then((info) => {
       console.log(info);
-      
-        trend_01.innerHTML =`${info.data[0]},`;
-        trend_02.innerHTML =`${info.data[2]},`;
-        trend_03.innerHTML =`${info.data[3]},`;
-        trend_01.innerHTML =`${info.data[4]},`;
-        trend_05.innerHTML =`${info.data[5]},`;
 
-      
-     
+      trend_01.innerHTML = `${info.data[0]},`;
+      trend_02.innerHTML = `${info.data[2]},`;
+      trend_03.innerHTML = `${info.data[3]},`;
+      trend_01.innerHTML = `${info.data[4]},`;
+      trend_05.innerHTML = `${info.data[5]},`;
+
+
+
     })
 
     .catch(() => {
